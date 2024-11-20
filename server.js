c6952bc2ae5a33e8c3cd9198ce306b99ba081d16
Bruno Habermann <habermann.bruno@gmail.com>
2024-11-19 22:24:58 -0300

    feature: Add get by Id

--- server.js
+++ server.js
@@ -6,17 +6,30 @@ app.use(express.json())
 const port = 3000;
 
 const posts = [
-    { description: 'Foto 1', image: 'https://http.cat/200.jpg' },
-    { description: 'Foto 2', image: 'https://http.cat/201.jpg' },
-    { description: 'Foto 3', image: 'https://http.cat/202.jpg' },
-    { description: 'Foto 4', image: 'https://http.cat/203.jpg' },
-    { description: 'Foto 5', image: 'https://http.cat/204.jpg' }
-]
+    { description: 'Foto 1', image: 'https://http.cat/200.jpg', id: 1 },
+    { description: 'Foto 2', image: 'https://http.cat/201.jpg', id: 2 },
+    { description: 'Foto 3', image: 'https://http.cat/202.jpg', id: 3 },
+    { description: 'Foto 4', image: 'https://http.cat/203.jpg', id: 4 },
+    { description: 'Foto 5', image: 'https://http.cat/204.jpg', id: 5 }
+  ]
+
+function buscarPostPorId(id) {
+     return posts.findIndex((post) => {
+        return post.id === Number(id);
+     });
+}
 
 app.listen(port, () => {
     console.log("Servidor escutando...");
 });
 
 app.get("/posts", (req, res) => {
-    res.status(200).send(posts);
+    res.status(200).json(posts);
+});
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorId(req.params.id);
+    let post = posts[index];
+    if (post == undefined) res.status(404).send('<img src="https://http.cat/404.jpg" alt="Imagem 404">');
+    res.status(200).json(post);
 });
